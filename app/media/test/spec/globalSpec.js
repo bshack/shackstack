b24c7b2757ed95755f967c32104864a24c29ec9a
William Shackelford <bshackelford@gmail.com>
2016-01-30 02:29:45 -0500

    wrap it

--- app/media/test/spec/globalSpec.js
+++ app/media/test/spec/globalSpec.js
@@ -1,17 +1,19 @@
-'use strict';
-var utilities = require('../../../../app/media/script/service/utilities.js');
-describe('Canary', function() {
-    it('is sucessfully running', function() {
-        expect(true).toBe(true);
+(function() {
+    'use strict';
+    var utilities = require('../../../../app/media/script/service/utilities.js');
+    describe('Canary', function() {
+        it('is sucessfully running', function() {
+            expect(true).toBe(true);
+        });
     });
-});
-describe('encoding html into entities', function() {
-    it('sucessfully formats the markup and text into entities', function() {
-        expect(utilities.htmlEncode('<&>')).toBe('&lt;&amp;&gt;');
+    describe('encoding html into entities', function() {
+        it('sucessfully formats the markup and text into entities', function() {
+            expect(utilities.htmlEncode('<&>')).toBe('&lt;&amp;&gt;');
+        });
     });
-});
-describe('decoding entities into markup', function() {
-    it('sucessfully formats the entities into markup and text', function() {
-        expect(utilities.htmlDecode('&lt;&amp;&gt;')).toBe('<&>');
+    describe('decoding entities into markup', function() {
+        it('sucessfully formats the entities into markup and text', function() {
+            expect(utilities.htmlDecode('&lt;&amp;&gt;')).toBe('<&>');
+        });
     });
-});
+})();
