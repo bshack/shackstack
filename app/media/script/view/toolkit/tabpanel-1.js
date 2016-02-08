ec024a3119f9dfdd862cc202e07b577d89c45185
William Shackelford <bshackelford@gmail.com>
2016-02-07 23:19:33 -0500

    tabpanel accessiblity

--- app/media/script/view/toolkit/tabpanel-1.js
+++ app/media/script/view/toolkit/tabpanel-1.js
@@ -6,6 +6,7 @@
             'click > [role=tablist] [role=tab]': 'tabClick',
             'keydown > [role=tablist] [role=tab]': 'tabKeydown'
         },
+        // for non screen reader users
         tabClick: function(e) {
             e.preventDefault();
             var $target = Backbone.$(e.target);
@@ -16,17 +17,21 @@
                 eventAction: 'click',
                 eventLabel: $target.text()
             });
+            // deselect all the tabs
             this.$el
                 .find('> [role=tablist] [role=tab]')
                 .attr('aria-selected', false)
                 .attr('tabindex', -1);
+            // select the tab click on
             $target
                 .attr('aria-selected', true)
                 .removeAttr('tabindex');
+            // deselect all the panels
             this.$el
                 .find('> [role=tabpanel]')
                 .attr('aria-hidden', true)
                 .attr('tabindex', -1);
+            // select the panel associated with the tab clicked on
             this.$el
                 .find('#' + $target.attr('aria-controls'))
                 .attr('aria-hidden', false)
@@ -34,37 +39,16 @@
         },
         tabKeydown: function(e) {
             var $target = Backbone.$(e.target);
-            if (e.which === 13) {
-                e.preventDefault();
+            // if spacebar key hit just trigger a click
+            if (e.which === 32) {
                 $target
                     .click();
-            } else if (e.which === 32) {
-                e.preventDefault();
-                //send click event to metrics
-                Backbone.Mediator.publish('metrics:event:send', {
-                    hitType: 'event',
-                    eventCategory: 'tabpanel',
-                    eventAction: 'spacebar',
-                    eventLabel: $target.text()
-                });
-                if ($target.attr('aria-selected') === true) {
-                    this.$el.find('> [role=tablist]')
-                        .find('[aria-selected=false]')
-                        .attr('aria-selected', true)
-                        .removeAttr('tabindex')
-                        .focus();
-                    $target
-                        .attr('aria-selected', false)
-                        .attr('tabindex', -1);
-                    this.$el.find('> [role=tabpanel]')
-                        .attr('aria-hidden', true)
-                        .attr('tabindex', -1);
-                    this.$el
-                        .find('#' + this.$el.find('> [role=tablist] [aria-selected=true]')
-                        .attr('aria-controls'))
-                        .attr('aria-hidden', false)
-                        .removeAttr('tabindex');
-                }
+            // if left arrow key
+            } else if (e.which === 37) {
+                $target.parent('li').prev().find('button').focus().trigger('click');
+            // if right arrow key
+            } else if (e.which === 39) {
+                $target.parent('li').next().find('button').focus().trigger('click');
             }
         }
     });
