66623cb5cceca8822eb55992b370a6c857397bb3
William Shackelford <bshackelford@gmail.com>
2016-03-15 22:40:17 -0400

    youtube player metrics

--- app/assets/script/view/toolkit/youtube-api-1.js
+++ app/assets/script/view/toolkit/youtube-api-1.js
@@ -3,7 +3,6 @@
     var Backbone = require('../../backbone/package');
     module.exports = Backbone.View.extend({
         initialize: function() {
-            var done = false;
             //load the api
             Backbone.$.ajax({
                 url: 'https://www.youtube.com/iframe_api',
@@ -12,32 +11,6 @@
             // This function creates an iframe (and YouTube player) after the API code downloads.
             this.el.onYouTubeIframeAPIReady = function(e) {
                 Backbone.Mediator.publish('youtube-api-1:state:ready', e);
-            }
-            // The API will call this function when the video player is ready.
-            this.el.onPlayerReady = function(e) {
-                // broadcast video is ready
-                Backbone.Mediator.publish('youtube-player-1:state:ready', e);
-            }
-            // The API calls this function when the player's state changes.
-            // The function indicates that when playing a video (state=1),
-            // the player should play for six seconds and then stop.
-            this.el.onPlayerStateChange = function(e) {
-                // broadcast video state change
-                Backbone.Mediator.publish('youtube-player-1:state:changed', e);
-                // broadcast video playing
-                if (e.data === 1) {
-                    Backbone.Mediator.publish('youtube-player-1:state:playing', e);
-                }
-                // broadcast video paused
-                if (e.data === 2) {
-                    Backbone.Mediator.publish('youtube-player-1:state:paused', e);
-                }
-                /*eslint-disable */
-                if (e.data === YT.PlayerState.PLAYING && !done) {
-                    setTimeout(e.target.stopVideo, 6000);
-                    done = true;
-                }
-                /*eslint-enable */
             }
         }
     });
--- app/assets/script/view/toolkit/youtube-player-1.js
+++ app/assets/script/view/toolkit/youtube-player-1.js
@@ -8,33 +8,88 @@
             this.$el.append('<div></div>');
         },
         subscriptions: {
-            'youtube-api-1:state:ready': 'eventApiReady',
-            'youtube-player-1:state:playing': 'eventVideoPlay'
+            'youtube-api-1:state:ready': 'eventApiReady'
         },
         eventApiReady: function() {
+            var _this = this;
             /*eslint-disable */
             this.player = new YT.Player(this.$el.find('div').get(0), Backbone.$.extend({
                 events: {
-                    'onReady': onPlayerReady,
-                    'onStateChange': onPlayerStateChange
+                    'onReady': _this.eventPlayerReady,
+                    'onStateChange': _this.eventStateChanged
                 }
             }, this.options));
             /*eslint-enable */
         },
-        eventVideoPlay: function(e) {
-            //only fire metrics for this video when there multiples on the page
-            if (!this.metricsPlayLogged &&
-                    e.target.getVideoData()['video_id'] === this.player.getVideoData()['video_id']) {
-                //just log one play per page load, sometimes play is triggered a lot when network congestion is present
-                this.metricsPlayLogged = true;
-                //log a panel view
+        eventPlayerReady: function(e) {
+            // broadcast video is ready
+            Backbone.Mediator.publish('youtube-player-1:state:ready', e);
+        },
+        eventStateChanged: function(e) {
+            window.console.log(e, e.target);
+            var done = false;
+            // broadcast video state change
+            Backbone.Mediator.publish('youtube-player-1:state:changed', e);
+            // broadcast video unstarted
+            if (e.data === -1) {
+                Backbone.Mediator.publish('youtube-player-1:state:unstarted', e);
+            }
+            // broadcast video ended
+            if (e.data === 0) {
+                Backbone.Mediator.publish('youtube-player-1:state:ended', e);
                 Backbone.Mediator.publish('metrics:event:send', {
                     hitType: 'event',
                     eventCategory: 'video',
-                    eventAction: 'play',
-                    eventLabel: this.player.getVideoData()['video_id']
+                    eventAction: 'ended',
+                    eventLabel: e.target.getVideoData()['video_id']
                 });
             }
+            // broadcast video playing
+            if (e.data === 1) {
+                Backbone.Mediator.publish('youtube-player-1:state:playing', e);
+                Backbone.Mediator.publish('metrics:event:send', {
+                    hitType: 'event',
+                    eventCategory: 'video',
+                    eventAction: 'playing',
+                    eventLabel: e.target.getVideoData()['video_id']
+                });
+            }
+            // broadcast video paused
+            if (e.data === 2) {
+                Backbone.Mediator.publish('youtube-player-1:state:paused', e);
+                Backbone.Mediator.publish('metrics:event:send', {
+                    hitType: 'event',
+                    eventCategory: 'video',
+                    eventAction: 'paused',
+                    eventLabel: e.target.getVideoData()['video_id']
+                });
+            }
+            // broadcast video buffering
+            if (e.data === 3) {
+                Backbone.Mediator.publish('youtube-player-1:state:buffering', e);
+                Backbone.Mediator.publish('metrics:event:send', {
+                    hitType: 'event',
+                    eventCategory: 'video',
+                    eventAction: 'buffering',
+                    eventLabel: e.target.getVideoData()['video_id']
+                });
+            }
+            // broadcast video cued
+            if (e.data === 5) {
+                Backbone.Mediator.publish('youtube-player-1:state:cued', e);
+                Backbone.Mediator.publish('metrics:event:send', {
+                    hitType: 'event',
+                    eventCategory: 'video',
+                    eventAction: 'cued',
+                    eventLabel: e.target.getVideoData()['video_id']
+                });
+            }
+            /*eslint-disable */
+            if (e.data === YT.PlayerState.PLAYING && !done) {
+                setTimeout(e.target.stopVideo, 6000);
+                done = true;
+            }
+            /*eslint-enable */
         }
     });
 })();
