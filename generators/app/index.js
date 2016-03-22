eb0e467175f5963908a52c81c52a4d55f80975f7
William Shackelford <bshackelford@gmail.com>
2016-03-22 15:08:14 -0400

    fix yoeman install

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,12 +7,12 @@ const yosay = require('yosay');
 
 // ## Setup
 module.exports = yeoman.Base.extend({
-    prompting: () => {
+    prompting: function() {
         this.log(yosay(
             'Setting up ' + chalk.red('generator-shackstack') + ' generator.'
         ));
     },
-    writing: () => {
+    writing: function() {
         //copy over the site files
         this.fs.copy(
           this.templatePath('../../../app/**'),
@@ -34,7 +34,7 @@ module.exports = yeoman.Base.extend({
           this.destinationPath('./')
         );
     },
-    install: () => {
+    install: function() {
         this.installDependencies({
             bower: false,
             npm: true
